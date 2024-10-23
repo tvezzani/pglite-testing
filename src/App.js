d1ea7b255ef4bc24a122c42e9f328b7e3d083188
tvezzani <tvezzani@users.noreply.github.com>
2024-10-23 02:18:06 -0600

    Adds loop for spinning up multiple instances of pglite

--- src/App.js
+++ src/App.js
@@ -34,6 +34,28 @@ ret = await db.query(`
 
 console.log("Example updated results: ", ret.rows);
 
+// Testing spinning up multiple PGlite instances
+for (let i = 0; i < 100; i++) {
+  const pg = new PGlite();
+  await pg.exec(`
+    CREATE TABLE IF NOT EXISTS todo (
+      id SERIAL PRIMARY KEY,
+      task TEXT,
+      done BOOLEAN DEFAULT false
+    );
+    INSERT INTO todo (task, done) VALUES ('Install PGlite from NPM', true);
+    INSERT INTO todo (task, done) VALUES ('Load PGlite', true);
+    INSERT INTO todo (task, done) VALUES ('Create a table', true);
+    INSERT INTO todo (task, done) VALUES ('Insert some data', true);
+    INSERT INTO todo (task) VALUES ('Update a task');
+  `)
+
+  let queryResults = await db.query(`
+    SELECT * from todo WHERE id = 1;
+  `)
+  console.log(queryResults);
+}
+
 function App() {
   return (
     <div className="App">
